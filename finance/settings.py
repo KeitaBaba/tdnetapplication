8570bcf61f96006528d17da717fc21c9549fdb56
KeitaBaba <keita.b0128@gmail.com>
2021-10-15 10:48:39 +0900

    update

--- finance/settings.py
+++ finance/settings.py
@@ -148,12 +148,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-
-import dj_database_url
-
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 import django_heroku
 django_heroku.settings(locals())
